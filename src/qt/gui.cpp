fdbcb04dec120e677db3279571cf4da1ef85ccbe
EduCoin <29717027+EduCoinTeam@users.noreply.github.com>
2019-09-22 13:28:00 -0700

    changes

--- src/qt/gui.cpp
+++ src/qt/gui.cpp
@@ -775,20 +775,20 @@ void GUI::setEncryptionStatus(int status)
         encryptionIcon.   addClass("fa-unlock");
         toggleLockIcon.removeClass("fa-unlock");
         toggleLockIcon.   addClass("fa-lock");
-        encryptionIcon   .setAttribute("src", "qrc:///assets/icons/lock_off");
+        encryptionIcon   .setAttribute("src", ":/assets/icons/lock_off");
         
         if (fWalletUnlockStakingOnly)
         {
             encryptionIcon   .setAttribute("data-title", tr("Wallet is <b>encrypted</b> and currently <b>unlocked</b> for staking only"));
             encryptionIcon.removeClass("vred");
             encryptionIcon.addClass("lightgrey");
-			encryptionIcon   .setAttribute("src", "qrc:///assets/icons/lock_stake");
+			encryptionIcon   .setAttribute("src", ":/assets/icons/lock_stake");
         } else
         {
             encryptionIcon   .setAttribute("data-title", tr("Wallet is <b>encrypted</b> and currently <b>unlocked</b>"));
             encryptionIcon.addClass("vred");
             encryptionIcon.removeClass("lightgrey");
-			encryptionIcon   .setAttribute("src", "qrc:///assets/icons/lock_off");
+			encryptionIcon   .setAttribute("src", ":/assets/icons/lock_off");
         };
         
         encryptButton.addClass("none");
@@ -807,7 +807,7 @@ void GUI::setEncryptionStatus(int status)
         toggleLockIcon.removeClass("fa-lock");
         toggleLockIcon.   addClass("fa-unlock");
         encryptionIcon   .setAttribute("data-title", tr("Wallet is <b>encrypted</b> and currently <b>locked</b>"));
-		encryptionIcon   .setAttribute("src", "qrc:///assets/icons/lock");
+		encryptionIcon   .setAttribute("src", ":/assets/icons/lock");
         
         encryptionIcon.addClass("vred");
         encryptionIcon.removeClass("lightgrey");
